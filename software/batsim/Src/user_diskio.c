e17e7a2dd81f82fe32d6ccacbafbb13e1f6788f0
Jan Käberich <kaeberic@ibr.cs.tu-bs.de>
2017-05-02 22:08:20 +0200

    FatFs user interface added

--- software/batsim/Src/user_diskio.c
+++ software/batsim/Src/user_diskio.c
@@ -63,9 +63,220 @@
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
 
+#define CS_H()		GPIOB->BSRR = GPIO_PIN_6	/* Set MMC CS "high" */
+#define CS_L()		GPIOB->BRR = GPIO_PIN_6	/* Set MMC CS "low" */
+
+/* MMC card type flags (MMC_GET_TYPE) */
+#define CT_MMC		0x01		/* MMC ver 3 */
+#define CT_SD1		0x02		/* SD ver 1 */
+#define CT_SD2		0x04		/* SD ver 2 */
+#define CT_SDC		(CT_SD1|CT_SD2)	/* SD */
+#define CT_BLOCK	0x08		/* Block addressing */
+
+/* MMC/SD command (SPI mode) */
+#define CMD0	(0)			/* GO_IDLE_STATE */
+#define CMD1	(1)			/* SEND_OP_COND */
+#define	ACMD41	(0x80+41)	/* SEND_OP_COND (SDC) */
+#define CMD8	(8)			/* SEND_IF_COND */
+#define CMD9	(9)			/* SEND_CSD */
+#define CMD10	(10)		/* SEND_CID */
+#define CMD12	(12)		/* STOP_TRANSMISSION */
+#define CMD13	(13)		/* SEND_STATUS */
+#define ACMD13	(0x80+13)	/* SD_STATUS (SDC) */
+#define CMD16	(16)		/* SET_BLOCKLEN */
+#define CMD17	(17)		/* READ_SINGLE_BLOCK */
+#define CMD18	(18)		/* READ_MULTIPLE_BLOCK */
+#define CMD23	(23)		/* SET_BLOCK_COUNT */
+#define	ACMD23	(0x80+23)	/* SET_WR_BLK_ERASE_COUNT (SDC) */
+#define CMD24	(24)		/* WRITE_BLOCK */
+#define CMD25	(25)		/* WRITE_MULTIPLE_BLOCK */
+#define CMD32	(32)		/* ERASE_ER_BLK_START */
+#define CMD33	(33)		/* ERASE_ER_BLK_END */
+#define CMD38	(38)		/* ERASE */
+#define CMD55	(55)		/* APP_CMD */
+#define CMD58	(58)		/* READ_OCR */
+
 /* Private variables ---------------------------------------------------------*/
+static BYTE CardType; /* b0:MMC, b1:SDv1, b2:SDv2, b3:Block addressing */
+extern SPI_HandleTypeDef hspi3;
 /* Disk status */
 static volatile DSTATUS Stat = STA_NOINIT;
+
+/*-----------------------------------------------------------------------*/
+/* Transmit bytes to the card (bitbanging)                               */
+/*-----------------------------------------------------------------------*/
+
+static
+void xmit_mmc(const BYTE* buff, /* Data to be sent */
+UINT bc /* Number of bytes to send */
+) {
+	HAL_SPI_Transmit(&hspi3, (uint8_t*) buff, bc, 10);
+}
+
+/*-----------------------------------------------------------------------*/
+/* Receive bytes from the card (bitbanging)                              */
+/*-----------------------------------------------------------------------*/
+
+static
+void rcvr_mmc(BYTE *buff, /* Pointer to read buffer */
+UINT bc /* Number of bytes to receive */
+) {
+	HAL_SPI_Receive(&hspi3, buff, bc, 10);
+}
+
+/*-----------------------------------------------------------------------*/
+/* Wait for card ready                                                   */
+/*-----------------------------------------------------------------------*/
+
+static
+int wait_ready(void) /* 1:OK, 0:Timeout */
+{
+	BYTE d;
+	UINT tmr;
+
+	for (tmr = 500; tmr; tmr--) { /* Wait for ready in timeout of 500ms */
+		rcvr_mmc(&d, 1);
+		if (d == 0xFF)
+			break;
+		HAL_Delay(1);
+	}
+
+	return tmr ? 1 : 0;
+}
+
+/*-----------------------------------------------------------------------*/
+/* Deselect the card and release SPI bus                                 */
+/*-----------------------------------------------------------------------*/
+
+static
+void deselect(void) {
+	BYTE d;
+
+	CS_H();
+	rcvr_mmc(&d, 1); /* Dummy clock (force DO hi-z for multiple slave SPI) */
+}
+
+/*-----------------------------------------------------------------------*/
+/* Select the card and wait for ready                                    */
+/*-----------------------------------------------------------------------*/
+
+static
+int select(void) /* 1:OK, 0:Timeout */
+{
+	BYTE d;
+
+	CS_L();
+	rcvr_mmc(&d, 1); /* Dummy clock (force DO enabled) */
+
+	if (wait_ready())
+		return 1; /* OK */
+	deselect();
+	return 0; /* Failed */
+}
+
+/*-----------------------------------------------------------------------*/
+/* Receive a data packet from the card                                   */
+/*-----------------------------------------------------------------------*/
+
+static
+int rcvr_datablock( /* 1:OK, 0:Failed */
+BYTE *buff, /* Data buffer to store received data */
+UINT btr /* Byte count */
+) {
+	BYTE d[2];
+	UINT tmr;
+
+	for (tmr = 100; tmr; tmr--) { /* Wait for data packet in timeout of 100ms */
+		rcvr_mmc(d, 1);
+		if (d[0] != 0xFF)
+			break;
+		HAL_Delay(1);
+	}
+	if (d[0] != 0xFE)
+		return 0; /* If not valid data token, return with error */
+
+	rcvr_mmc(buff, btr); /* Receive the data block into buffer */
+	rcvr_mmc(d, 2); /* Discard CRC */
+
+	return 1; /* Return with success */
+}
+
+/*-----------------------------------------------------------------------*/
+/* Send a data packet to the card                                        */
+/*-----------------------------------------------------------------------*/
+
+static
+int xmit_datablock( /* 1:OK, 0:Failed */
+const BYTE *buff, /* 512 byte data block to be transmitted */
+BYTE token /* Data/Stop token */
+) {
+	BYTE d[2];
+
+	if (!wait_ready())
+		return 0;
+
+	d[0] = token;
+	xmit_mmc(d, 1); /* Xmit a token */
+	if (token != 0xFD) { /* Is it data token? */
+		xmit_mmc(buff, 512); /* Xmit the 512 byte data block to MMC */
+		rcvr_mmc(d, 2); /* Xmit dummy CRC (0xFF,0xFF) */
+		rcvr_mmc(d, 1); /* Receive data response */
+		if ((d[0] & 0x1F) != 0x05) /* If not accepted, return with error */
+			return 0;
+	}
+
+	return 1;
+}
+
+/*-----------------------------------------------------------------------*/
+/* Send a command packet to the card                                     */
+/*-----------------------------------------------------------------------*/
+
+static BYTE send_cmd( /* Returns command response (bit7==1:Send failed)*/
+BYTE cmd, /* Command byte */
+DWORD arg /* Argument */
+) {
+	BYTE n, d, buf[6];
+
+	if (cmd & 0x80) { /* ACMD<n> is the command sequense of CMD55-CMD<n> */
+		cmd &= 0x7F;
+		n = send_cmd(CMD55, 0);
+		if (n > 1)
+			return n;
+	}
+
+	/* Select the card and wait for ready except to stop multiple block read */
+	if (cmd != CMD12) {
+		deselect();
+		if (!select())
+			return 0xFF;
+	}
+
+	/* Send a command packet */
+	buf[0] = 0x40 | cmd; /* Start + Command index */
+	buf[1] = (BYTE) (arg >> 24); /* Argument[31..24] */
+	buf[2] = (BYTE) (arg >> 16); /* Argument[23..16] */
+	buf[3] = (BYTE) (arg >> 8); /* Argument[15..8] */
+	buf[4] = (BYTE) arg; /* Argument[7..0] */
+	n = 0x01; /* Dummy CRC + Stop */
+	if (cmd == CMD0)
+		n = 0x95; /* (valid CRC for CMD0(0)) */
+	if (cmd == CMD8)
+		n = 0x87; /* (valid CRC for CMD8(0x1AA)) */
+	buf[5] = n;
+	xmit_mmc(buf, 6);
+
+	/* Receive command response */
+	if (cmd == CMD12)
+		rcvr_mmc(&d, 1); /* Skip a stuff byte when stop reading */
+	n = 10; /* Wait for a valid response in timeout of 10 attempts */
+	do
+		rcvr_mmc(&d, 1);
+	while ((d & 0x80) && --n);
+
+	return d; /* Return with the response value */
+}
+
 
 /* USER CODE END DECL */
 
@@ -106,8 +317,57 @@ DSTATUS USER_initialize (
 )
 {
   /* USER CODE BEGIN INIT */
-    Stat = STA_NOINIT;
-    return Stat;
+	BYTE n, ty, cmd, buf[4];
+	UINT tmr;
+	DSTATUS s;
+
+	if (pdrv)
+		return RES_NOTRDY;
+
+	CS_H();
+
+	for (n = 10; n; n--)
+		rcvr_mmc(buf, 1); /* Apply 80 dummy clocks and the card gets ready to receive command */
+
+	ty = 0;
+	if (send_cmd(CMD0, 0) == 1) { /* Enter Idle state */
+		if (send_cmd(CMD8, 0x1AA) == 1) { /* SDv2? */
+			rcvr_mmc(buf, 4); /* Get trailing return value of R7 resp */
+			if (buf[2] == 0x01 && buf[3] == 0xAA) { /* The card can work at vdd range of 2.7-3.6V */
+				for (tmr = 1000; tmr; tmr--) { /* Wait for leaving idle state (ACMD41 with HCS bit) */
+					if (send_cmd(ACMD41, 1UL << 30) == 0)
+						break;
+					HAL_Delay(1);
+				}
+				if (tmr && send_cmd(CMD58, 0) == 0) { /* Check CCS bit in the OCR */
+					rcvr_mmc(buf, 4);
+					ty = (buf[0] & 0x40) ? CT_SD2 | CT_BLOCK : CT_SD2; /* SDv2 */
+				}
+			}
+		} else { /* SDv1 or MMCv3 */
+			if (send_cmd(ACMD41, 0) <= 1) {
+				ty = CT_SD1;
+				cmd = ACMD41; /* SDv1 */
+			} else {
+				ty = CT_MMC;
+				cmd = CMD1; /* MMCv3 */
+			}
+			for (tmr = 1000; tmr; tmr--) { /* Wait for leaving idle state */
+				if (send_cmd(cmd, 0) == 0)
+					break;
+				HAL_Delay(1);
+			}
+			if (!tmr || send_cmd(CMD16, 512) != 0) /* Set R/W block length to 512 */
+				ty = 0;
+		}
+	}
+	CardType = ty;
+	s = ty ? 0 : STA_NOINIT;
+	Stat = s;
+
+	deselect();
+
+	return s;
   /* USER CODE END INIT */
 }
 
@@ -121,7 +381,8 @@ DSTATUS USER_status (
 )
 {
   /* USER CODE BEGIN STATUS */
-    Stat = STA_NOINIT;
+	if (pdrv)
+		return STA_NOINIT;
     return Stat;
   /* USER CODE END STATUS */
 }
@@ -142,7 +403,28 @@ DRESULT USER_read (
 )
 {
   /* USER CODE BEGIN READ */
-    return RES_OK;
+	if (disk_status(pdrv) & STA_NOINIT)
+		return RES_NOTRDY;
+	if (!(CardType & CT_BLOCK))
+		sector *= 512; /* Convert LBA to byte address if needed */
+
+	if (count == 1) { /* Single block read */
+		if ((send_cmd(CMD17, sector) == 0) /* READ_SINGLE_BLOCK */
+		&& rcvr_datablock(buff, 512))
+			count = 0;
+	} else { /* Multiple block read */
+		if (send_cmd(CMD18, sector) == 0) { /* READ_MULTIPLE_BLOCK */
+			do {
+				if (!rcvr_datablock(buff, 512))
+					break;
+				buff += 512;
+			} while (--count);
+			send_cmd(CMD12, 0); /* STOP_TRANSMISSION */
+		}
+	}
+	deselect();
+
+	return count ? RES_ERROR : RES_OK;
   /* USER CODE END READ */
 }
 
@@ -164,7 +446,31 @@ DRESULT USER_write (
 { 
   /* USER CODE BEGIN WRITE */
   /* USER CODE HERE */
-    return RES_OK;
+	if (disk_status(pdrv) & STA_NOINIT)
+		return RES_NOTRDY;
+	if (!(CardType & CT_BLOCK))
+		sector *= 512; /* Convert LBA to byte address if needed */
+
+	if (count == 1) { /* Single block write */
+		if ((send_cmd(CMD24, sector) == 0) /* WRITE_BLOCK */
+		&& xmit_datablock(buff, 0xFE))
+			count = 0;
+	} else { /* Multiple block write */
+		if (CardType & CT_SDC)
+			send_cmd(ACMD23, count);
+		if (send_cmd(CMD25, sector) == 0) { /* WRITE_MULTIPLE_BLOCK */
+			do {
+				if (!xmit_datablock(buff, 0xFC))
+					break;
+				buff += 512;
+			} while (--count);
+			if (!xmit_datablock(0, 0xFD)) /* STOP_TRAN token */
+				count = 1;
+		}
+	}
+	deselect();
+
+	return count ? RES_ERROR : RES_OK;
   /* USER CODE END WRITE */
 }
 #endif /* _USE_WRITE == 1 */
@@ -184,8 +490,49 @@ DRESULT USER_ioctl (
 )
 {
   /* USER CODE BEGIN IOCTL */
-    DRESULT res = RES_ERROR;
-    return res;
+	DRESULT res;
+	BYTE n, csd[16];
+	DWORD cs;
+
+	if (disk_status(pdrv) & STA_NOINIT)
+		return RES_NOTRDY; /* Check if card is in the socket */
+
+	res = RES_ERROR;
+	switch (cmd) {
+	case CTRL_SYNC: /* Make sure that no pending write process */
+		if (select())
+			res = RES_OK;
+		break;
+
+	case GET_SECTOR_COUNT: /* Get number of sectors on the disk (DWORD) */
+		if ((send_cmd(CMD9, 0) == 0) && rcvr_datablock(csd, 16)) {
+			if ((csd[0] >> 6) == 1) { /* SDC ver 2.00 */
+				cs = csd[9] + ((WORD) csd[8] << 8)
+						+ ((DWORD) (csd[7] & 63) << 16) + 1;
+				*(DWORD*) buff = cs << 10;
+			} else { /* SDC ver 1.XX or MMC */
+				n = (csd[5] & 15) + ((csd[10] & 128) >> 7) + ((csd[9] & 3) << 1)
+						+ 2;
+				cs = (csd[8] >> 6) + ((WORD) csd[7] << 2)
+						+ ((WORD) (csd[6] & 3) << 10) + 1;
+				*(DWORD*) buff = cs << (n - 9);
+			}
+			res = RES_OK;
+		}
+		break;
+
+	case GET_BLOCK_SIZE: /* Get erase block size in unit of sector (DWORD) */
+		*(DWORD*) buff = 128;
+		res = RES_OK;
+		break;
+
+	default:
+		res = RES_PARERR;
+	}
+
+	deselect();
+
+	return res;
   /* USER CODE END IOCTL */
 }
 #endif /* _USE_IOCTL == 1 */
